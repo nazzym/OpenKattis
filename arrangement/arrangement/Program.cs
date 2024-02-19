220a1419c42b392aafd5f66ea66d9aadd0fbb421
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-19 21:54:59 +0000

    doneeee:)

---
+++ arrangement/arrangement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Myreality
+{
+    public class Myworld
+    {
+        public static void Main()
+        {
+            int rooms = int.Parse(Console.ReadLine());
+            int people = int.Parse(Console.ReadLine());
+            int teams = people / rooms;
+            int remainder = people % rooms;
+            for (int i = 0; i < rooms; i++)
+            {
+                for (int j = 0; j < teams; j++)
+                {
+                    Console.Write("*");
+                }
+                if (remainder > 0)
+                {
+                    Console.Write("*");
+                    remainder--;
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+    }
+     
