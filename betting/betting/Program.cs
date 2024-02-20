a9e63f51194b57ec3241ce2ddbfe51733bfe8dfe
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-20 15:40:32 +0000

    doneeeee ;)

---
+++ betting/betting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace myreality
+{
+    class Bet
+    {
+        static void Main ()
+
+        {
+            string input = Console.ReadLine();
+            float a = int.Parse(input);
+            a = 100 - a;
+            Console.WriteLine(1 + a / (100 - a));
+            a = 100 - a;
+            Console.WriteLine(1 + a / (100 - a));
+        }
+    }
+}
