b5d87ee5f7eb0973b41c211eeabadf230d4db5ee
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-20 23:51:30 +0000

    donee:)

---
+++ ovissa/ovissa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Myworld
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        int uncertainty = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == 'u')
+            {
+                uncertainty++;
+            }
+        }
+
+        Console.WriteLine(uncertainty);
+    }
+}
