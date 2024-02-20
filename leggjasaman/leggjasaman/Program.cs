13df29def179b6c81d806034a672e04f3d34de49
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-20 23:37:35 +0000

    doneee:)

---
+++ leggjasaman/leggjasaman/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace leggjasaman
+{
+    class Myworld
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+    }
+    
+}
+
---
+++ metronome/metronome/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace myworld
+{
+    class Mygame
+    {
+        static void Main()
+        {
+            decimal song = decimal.Parse(Console.ReadLine());
+            decimal result = song / 4;
+            
+            Console.WriteLine(result);
+        }
+    }
+}
