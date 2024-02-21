fadc0af2cec832540cf4ec841271a7467a0fbcac
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-21 00:47:10 +0000

    doneee:))))

---
+++ fifa/fifa/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace fifa
+{
+    public class Myreality
+    {
+        static void Main()
+        {
+            int input1 = int.Parse(Console.ReadLine());
+            int input2 = int.Parse(Console.ReadLine());
+            int result = 2022 + (input1) / input2;
+            Console.WriteLine(result);
+        }
+    }
+}
