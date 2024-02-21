719063a6fb24f3e130f6766f5c986e00365ca319
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-21 00:30:08 +0000

    donee:))

---
+++ flatbokuveisla/flatbokuveisla/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace flatbokuveisla
+{
+    public class Myreality
+    {
+        public static void Main()
+        {
+            int input1 = int.Parse(Console.ReadLine());
+            int input2 = int.Parse(Console.ReadLine());
+            int remainder, quotient = Math.DivRem(input1,input2, out remainder);
+            Console.WriteLine(remainder);
+        }
+    }
+}
