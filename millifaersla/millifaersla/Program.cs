785f61c216681b8a63a2b3899fa4143374f40b17
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-21 01:53:57 +0000

    doneeee

---
+++ millifaersla/millifaersla/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int numstones = int.Parse(Console.ReadLine());
+while  ( numstones > 1)
+    if (numstones % 2 == 0) 
+             {
+
+        numstones -= 2;
+            }
+else
+    {
+        numstones -= 1;
+    }
+string winner;
+if (numstones == 1)
+    winner = "Alice";
+else 
+    winner = "Bob";
+Console.WriteLine(winner);
+
---
+++ twostones/twostones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace twostones
+{
+    public class Myreality
+    {
+        public static void Main()
+        {
+            int numstones = int.Parse(Console.ReadLine());
+            string winner = "";
+            while (numstones > 1)
+            {
+                if (numstones % 2 == 0)
+                {
+                    numstones -= 2;
+                    winner = "Bob";
+                }
+                else
+                {
+                    numstones = 1;
+                    winner = "Alice";
+                }
+            }
+
+            if (numstones == 1)
+            {
+                winner = "Alice";
+            }
+
+            Console.WriteLine(winner);
+        }
+    }
+}
