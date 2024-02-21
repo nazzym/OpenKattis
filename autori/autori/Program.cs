ea059982b644461c5b43b0f373d7ffd4360be6f3
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-21 02:18:57 +0000

    donee:)

---
+++ autori/autori/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShortenNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] names = input.Split('-');
+            string shortName = "";
+
+            foreach (string name in names)
+            {
+                shortName += name[0];
+            }
+
+            Console.WriteLine(shortName);
+        }
+    }
+}
