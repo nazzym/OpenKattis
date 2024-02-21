e40a9a2432a48c67106530500c6ffa79c11cf38e
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-21 02:14:00 +0000

    donee:)

---
+++ AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace addtwonumbers
+{
+    public class Myreality
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int num1 = int.Parse(numbers[0]);
+            int num2 = int.Parse(numbers[1]);
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+    }
+}
