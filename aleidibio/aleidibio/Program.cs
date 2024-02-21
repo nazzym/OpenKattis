29c9815a0b4109f6300987b71d328a7218194370
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-21 00:03:39 +0000

    done:))

---
+++ aleidibio/aleidibio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace aleidibio
+{
+    class Myreality
+    {
+        public static void Main()
+        {
+            int line1 = int.Parse(Console.ReadLine());
+            int line2 = int.Parse(Console.ReadLine());
+            int line3 = int.Parse(Console.ReadLine());
+            int result = (line3) - (line1 + line2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
