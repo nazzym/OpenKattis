d2d90a7430e81677aa3116c00bfcb071d18fe6ec
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-22 01:33:11 +0000

    dobrrr

---
+++ ameriskur/ameriskur/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ameriskur
+{
+    public class Myreality
+    {
+        static void Main()
+        {
+            decimal num = decimal.Parse(Console.ReadLine());
+         decimal  result = num * 0.09144m ;
+            Console.WriteLine(result);
+        }
+    }
+}
