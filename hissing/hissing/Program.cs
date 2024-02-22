6f326da0d8484f5d6c1fa5cf7ccfe3f37917c98b
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-22 16:21:35 +0000

    doneeee

---
+++ hissing/hissing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace hissingmicrophone
+{
+    public class Myreality
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine();
+            string hiss = "ss";
+            if ( word.Contains(hiss))// this is too look for the ss so it can print out hiss
+            {
+                Console.WriteLine("hiss");
+            }
+            else
+            {
+                Console.WriteLine("no hiss");
+            }
+
+        }
+    }
+}
