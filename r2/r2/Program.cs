f7db07d8c416554be3a4d5d9459c3a2b30eb0f8f
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-22 02:24:50 +0000

    done

---
+++ r2/r2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace r2
+{
+    public class Myreality
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] num = input.Split(' ');
+
+            if (num.Length != 2)
+            {
+                Console.WriteLine("Invalid input. Please enter two numbers separated by a space.");
+            }
+            else
+            {
+                int n1 = int.Parse(num[0]);
+                int n2 = int.Parse(num[1]);
+                int result = n2 * 2 - n1;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ timeloop/timeloop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace timeloop
+{
+    public class Myreality
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i + " Abracadabra");
+            }
+        }
+    }
+}
