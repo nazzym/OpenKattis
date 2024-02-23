966a59bd9f4e94c9044f0fe91fc5eb0d4cac9ab3
nazzym <S.MOHAMED-2021@hull.ac.uk>
2024-02-23 00:22:34 +0000

    not sure but somehow passed the testsdonee

---
+++ chanukah/chanukah/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace chanukah
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int datasets = int.Parse(Console.ReadLine()); // Total datasets
+
+            for (int dataset = 0; dataset < datasets; dataset++)
+            {
+                string input = Console.ReadLine();
+                string[] parts = input.Split(' ');
+                int holidayNumber = int.Parse(parts[0]);
+                int days = int.Parse(parts[1]);
+
+                int totalCandles = days * (days + 1) / 2 + days; // Total candles needed
+
+                string output = $"{holidayNumber.ToString().PadLeft(5)} {totalCandles.ToString().PadLeft(5)}";
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ qaly/qaly/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Myreality
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());// convert to number
+
+        decimal[][] periods = new decimal[n][];// two dimensional array with decimal values named period
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] line = Console.ReadLine().Split();// splits into an array of strings using the whitespace 
+            periods[i] = new decimal[] { decimal.Parse(line[0]), decimal.Parse(line[1]) };
+        }
+
+        decimal qalys = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            qalys += periods[i][0] * periods[i][1];
+        }
+
+        Console.WriteLine(qalys);
+
+
+    }
+}
