f0d9d885226fc41b5d1df9ca55343d58c1f140bf
nazzym <146441501+nazzym@users.noreply.github.com>
2024-03-12 16:01:54 +0000

    doneUpdate Program.cs

--- chanukah/chanukah/Program.cs
+++ chanukah/chanukah/Program.cs
@@ -1,23 +1,75 @@
-﻿namespace chanukah
+﻿using System;
+
+namespace Chanukah
 {
     internal class Program
     {
         static void Main()
         {
-            int datasets = int.Parse(Console.ReadLine()); // Total datasets
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+            int numHolidays = int.Parse(Console.ReadLine());
+            int[] candlesPerDay = new int[numHolidays];
 
-            for (int dataset = 0; dataset < datasets; dataset++)
+            for (int i = 0; i < numHolidays; i++)
             {
-                string input = Console.ReadLine();
-                string[] parts = input.Split(' ');
-                int holidayNumber = int.Parse(parts[0]);
-                int days = int.Parse(parts[1]);
-
-                int totalCandles = days * (days + 1) / 2 + days; // Total candles needed
-
-                string output = $"{holidayNumber.ToString().PadLeft(5)} {totalCandles.ToString().PadLeft(5)}";
-                Console.WriteLine(output);
+                string[] line = Console.ReadLine().Split(' ');
+                int day = int.Parse(line[0]);
+                int candles = int.Parse(line[1]);
+                candlesPerDay[day - 1] = candles;
             }
+
+            int totalCandles = 0;
+            for (int i = 0; i < numHolidays; i++)
+            {
+                totalCandles += candlesPerDay[i];
+                Console.WriteLine($"{i + 1} {totalCandles}");
+            }
+=======
+            int holidays = int.Parse(Console.ReadLine()); // Total datasets
+  string[] days = new string[holidays]; 
+  for (int i = 0; i < holidays; i++) 
+  {
+      Console.WriteLine("please enter the amount of days of this holiday");
+      days[i] = Console.ReadLine();
+
+=======
+            int holidays = int.Parse(Console.ReadLine()); // Total datasets
+  string[] days = new string[holidays]; 
+  for (int i = 0; i < holidays; i++) 
+  {
+      Console.WriteLine("please enter the amount of days of this holiday");
+      days[i] = Console.ReadLine();
+
+>>>>>>> 1bfde2dd4ef8c044786475a9b4db4fa49fbb927c
+=======
+            int holidays = int.Parse(Console.ReadLine()); // Total datasets
+  string[] days = new string[holidays]; 
+  for (int i = 0; i < holidays; i++) 
+  {
+      Console.WriteLine("please enter the amount of days of this holiday");
+      days[i] = Console.ReadLine();
+
+>>>>>>> 1bfde2dd4ef8c044786475a9b4db4fa49fbb927c
+      int count = 1;
+     for ( int j = 1;  j > count; j++ )
+      {
+
+      Console.WriteLine(count);
+      }
+      
+      
+    
+      
+  }
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1bfde2dd4ef8c044786475a9b4db4fa49fbb927c
+=======
+>>>>>>> 1bfde2dd4ef8c044786475a9b4db4fa49fbb927c
+=======
+>>>>>>> 1bfde2dd4ef8c044786475a9b4db4fa49fbb927c
         }
     }
 }
---
+++ kikiiboba/kikiiboba/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.Design;
+
+namespace kikiiboba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //read line of text of user
+            string input = Console.ReadLine();
+
+            int bCount = 0;
+            int kCount = 0;
+
+            foreach (char c in input)
+            {
+                if (char.ToLower(c) == 'b')
+                {
+                    bCount++;
+                }
+                else if (char.ToLower(c) == 'k')
+                {
+                    kCount++;
+                }
+            } /// vfreorjkd
+
+            if (bCount > kCount)
+            {
+                Console.WriteLine("boba");
+            }
+            else if ((bCount <= 0) && (kCount <= 0)) 
+            {
+                Console.WriteLine("none");
+            }
+            else if (kCount > bCount)
+            {
+                Console.WriteLine("kiki");
+            }
+            else if (bCount == kCount)
+            {
+                Console.WriteLine("boki");
+            }
+           
+
+
+            // wokrs
+        }
+
+    }
+    
+}
